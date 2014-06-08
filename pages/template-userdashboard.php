686d919db8d39e17a7151b0a3352a2c7535ae377
siemvaessen <siem@zimmermanzimmerman.nl>
2014-06-08 17:47:26 +0300

    Started dasboard template page
    Seperate styles for dashboard css in the all.css

---
+++ pages/template-userdashboard.php
@@ -0,0 +1,341 @@
+<?php
+/*
+Template Name: City compare
+*/
+get_header(); the_post(); ?>
+	<div id="main">
+	
+
+ 
+ 		<div class="main-container">
+			<section class="main-block">
+				<header class="heading-container">
+					<div class="container-dashboard">
+						<h1>My city infographics</h1>
+						<p>Manage your creations, modify and share</p>
+					</div>	
+				</header>
+				
+		
+
+		<div class="area-columns">
+			<!-- container-columns -->
+			<div class="container-columns">
+				<div class="column">
+					<div class="col-holder">
+						<div class="col">
+							<!-- container-box -->
+							<section class="container-box">
+								<header class="heading-holder">
+									<h3>Proportion of Land Allocated to Street, Street Density</h3>
+								</header>
+								<div class="box-content">
+									<div class="widget">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/img28.png" alt="">
+									</div>
+								</div>
+<!--Start minicontroller -->
+			<div class="container-sort">
+				<div class="sort-holder">
+					<ul class="action-list">
+						<li><a href="#"></i>REMOVE</a></li>
+							<li><a href="#"></i>MODIFY</a></li>
+						 </div>
+					</div>
+<!--End minicontroller -->						
+							</section>
+						</div>
+						<div class="col">
+							<!-- container-box -->
+							<section class="container-box">
+								<header class="heading-holder">
+									<h3>Create another infographic</h3>
+								</header>
+								<div class="box-content">
+									<div class="widget">
+									</div>
+								</div>
+								<!--Start minicontroller -->
+			<div class="container-sort">
+				<div class="sort-holder">
+					<ul class="action-list">
+						<li><a href="#"></i>REMOVE</a></li>
+							<li><a href="#"></i>MODIFY</a></li>
+						 </div>
+					</div>
+<!--End minicontroller -->	
+							</section>
+						</div>
+					</div>
+				</div>
+				<div class="column">
+					<div class="col-holder">
+						<div class="col">
+							<!-- container-box -->
+							<section class="container-box">
+								<header class="heading-holder">
+									<h3>Create another infographic</h3>
+								</header>
+								<div class="box-content">
+									<div class="widget">
+									</div>
+								</div>
+								<!--Start minicontroller -->
+			<div class="container-sort">
+				<div class="sort-holder">
+					<ul class="action-list">
+						<li><a href="#"></i>REMOVE</a></li>
+							<li><a href="#"></i>MODIFY</a></li>
+						 </div>
+					</div>
+<!--End minicontroller -->	
+							</section>
+						</div>
+						<div class="col">
+							<!-- container-box -->
+							<section class="container-box">
+								<header class="heading-holder">
+									<h3>Create another infographic</h3>
+								</header>
+								<div class="box-content">
+									<div class="widget">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/img29.png" alt="">
+									</div>
+								</div>
+								<!--Start minicontroller -->
+			<div class="container-sort">
+				<div class="sort-holder">
+					<ul class="action-list">
+						<li><a href="#"></i>REMOVE</a></li>
+							<li><a href="#"></i>MODIFY</a></li>
+						 </div>
+					</div>
+<!--End minicontroller -->	
+							</section>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			
+			
+			
+						<section class="main-block">
+				<header class="heading-container">
+					<div class="container-dashboard">
+						<h1>My favorite city data</h1>
+						<p>Review and share favourited data</p>
+					</div>	
+				</header>
+			
+			
+			
+			<!-- container-columns -->
+			<div class="container-columns">
+				<div class="column">
+					<div class="col-holder">
+						<div class="col">
+							<!-- container-box -->
+							<section class="container-box">
+								<header class="heading-holder">
+									<h3>City population size and rate of change</h3>
+								</header>
+								<div class="box-content">
+									<div class="widget">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/img31.png" alt="">
+									</div>
+								</div>
+								<!--Start minicontroller -->
+			<div class="container-sort">
+				<div class="sort-holder">
+					<ul class="action-list">
+						<li><a href="#"></i>REMOVE</a></li>
+							<li><a href="#"></i>MODIFY</a></li>
+						 </div>
+					</div>
+<!--End minicontroller -->	
+							</section>
+						</div>
+						<div class="col">
+							<!-- container-box -->
+							<section class="container-box">
+								<header class="heading-holder">
+									<h3>CCity Population density</h3>
+								</header>
+								<div class="box-content">
+									<div class="widget">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/img32.png" alt="">
+									</div>
+								</div>
+								<!--Start minicontroller -->
+			<div class="container-sort">
+				<div class="sort-holder">
+					<ul class="action-list">
+						<li><a href="#"></i>REMOVE</a></li>
+							<li><a href="#"></i>MODIFY</a></li>
+						 </div>
+					</div>
+<!--End minicontroller -->	
+							</section>
+						</div>
+					</div>
+				</div>
+				<div class="column">
+					<div class="col-holder">
+						<div class="col">
+							<!-- container-box -->
+							<section class="container-box">
+								<header class="heading-holder">
+									<h3>City population size and rate of change</h3>
+								</header>
+								<div class="box-content">
+									<div class="widget">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/img33.png" alt="">
+									</div>
+								</div>
+								<!--Start minicontroller -->
+			<div class="container-sort">
+				<div class="sort-holder">
+					<ul class="action-list">
+						<li><a href="#"></i>REMOVE</a></li>
+							<li><a href="#"></i>MODIFY</a></li>
+						 </div>
+					</div>
+<!--End minicontroller -->	
+							</section>
+						</div>
+						<div class="col">
+							<!-- container-box -->
+							<section class="container-box">
+								<header class="heading-holder">
+									<h3>City Population density</h3>
+								</header>
+								<div class="box-content">
+									<div class="widget">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/img32.png" alt="">
+									</div>
+								</div>
+								<!--Start minicontroller -->
+			<div class="container-sort">
+				<div class="sort-holder">
+					<ul class="action-list">
+						<li><a href="#"></i>REMOVE</a></li>
+							<li><a href="#"></i>MODIFY</a></li>
+						 </div>
+					</div>
+<!--End minicontroller -->	
+							</section>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- container-columns -->
+			<div class="container-columns">
+				<div class="column">
+					<div class="col-holder">
+						<div class="col">
+							<!-- container-box -->
+							<section class="container-box">
+								<header class="heading-holder">
+									<h3>City Prosperity Index and Components</h3>
+								</header>
+								<div class="box-content">
+									<div class="widget">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/img34.jpg" alt="">
+									</div>
+								</div>
+								<!--Start minicontroller -->
+			<div class="container-sort">
+				<div class="sort-holder">
+					<ul class="action-list">
+						<li><a href="#"></i>REMOVE</a></li>
+							<li><a href="#"></i>MODIFY</a></li>
+						 </div>
+					</div>
+<!--End minicontroller -->	
+							</section>
+						</div>
+						<div class="col">
+							<!-- container-box -->
+							<section class="container-box">
+								<header class="heading-holder">
+									<h3>Urban slum population</h3>
+								</header>
+								<div class="box-content">
+									<div class="widget">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/img35.png" alt="">
+									</div>
+								</div>
+								<!--Start minicontroller -->
+			<div class="container-sort">
+				<div class="sort-holder">
+					<ul class="action-list">
+						<li><a href="#"></i>REMOVE</a></li>
+							<li><a href="#"></i>MODIFY</a></li>
+						 </div>
+					</div>
+<!--End minicontroller -->	
+							</section>
+						</div>
+					</div>
+				</div>
+				<div class="column">
+					<div class="col-holder">
+						<div class="col">
+							<!-- container-box -->
+							<section class="container-box">
+								<header class="heading-holder">
+									<h3>City Prosperity Index and Components</h3>
+								</header>
+								<div class="box-content">
+									<div class="widget">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/img34.jpg" alt="">
+									</div>
+								</div>
+								<!--Start minicontroller -->
+			<div class="container-sort">
+				<div class="sort-holder">
+					<ul class="action-list">
+						<li><a href="#"></i>REMOVE</a></li>
+							<li><a href="#"></i>MODIFY</a></li>
+						 </div>
+					</div>
+<!--End minicontroller -->	
+							</section>
+						</div>
+						<div class="col">
+							<!-- container-box -->
+							<section class="container-box">
+								<header class="heading-holder">
+									<h3>Urban slum population</h3>
+								</header>
+								<div class="box-content">
+									<div class="widget">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/img35.png" alt="">
+									</div>
+								</div>
+								
+<!--Start minicontroller -->
+			<div class="container-sort">
+				<div class="sort-holder">
+					<ul class="action-list">
+						<li><a href="#"></i>REMOVE</a></li>
+							<li><a href="#"></i>MODIFY</a></li>
+						 </div>
+					</div>
+<!--End minicontroller -->	
+		
+		
+		
+		
+		
+								
+				
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		</div>
+<?php get_footer(); ?>
