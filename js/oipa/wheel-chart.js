a4d358a08367f1c30a54771b30f1730e32ccc88d
Nick Garanko <ngaranko@gmail.com>
2014-09-25 17:20:09 +0200

    Added missing files

---
+++ js/oipa/wheel-chart.js
@@ -0,0 +1,125 @@
+function OipaWheelChart(id, options) {
+    var self = this;
+    self.year = 2013;
+    OipaRadarChart.call(self, id, options);
+
+    self.get_url = function() {
+        var _url = search_url + 'indicator-data/?format=json';
+        var _url_data = {
+            cities__in: self._initial_selection.left.cities.concat(self._initial_selection.right.cities),
+            indicators__in: self._initial_selection.left.indicators
+        }
+
+        return _url + '&' + $.map(_url_data, function(value, key) {
+            return key + '=' + $.map(value, function(a, _) { return a.id; }).join(',');
+        }).join('&');
+    }
+
+    self.create_html_skeleton = function() {
+        // Register event in event bus
+        OipaWidgetsBus.add_listener(self);
+
+        // create html
+        var html = '<li id="visualization_' + self.indicator + '">';
+        html += '<section class="container-box" data-vis-type="'+self.type+'" data-indicator="'+self.indicator+'">';
+        html += '<div class="box-content">';
+        html +=  '<div id="legend"></div>'
+        html +=  '<div class="widget" data-indicator="'+self.indicator+'">';
+        html +=    '<canvas height="900" width="900"></canvas>';
+        html +=  '</div>';
+
+        html += '</div></section></li>';
+
+        $(self.chartwrapper).append(html);
+    }
+
+    self.visualize = function(data) {
+        var _keys = Object.keys(data);
+        var _cities = self._initial_selection.left.cities.concat(self._initial_selection.right.cities);
+
+        var chart_data = {
+            labels: $.map(_keys, function(key) {
+                return data[key].indicator_friendly;
+            }),
+            datasets: $.map(_cities, function(city, num) {
+                var _color = num ? "220,220,220" : "151,187,205";
+
+                return {
+                    label: data[_keys[0]].locs[city.id].name,
+                    fillColor: "rgba(" + _color + ",0.2)",
+                    strokeColor: "rgba(" + _color + ",1)",
+                    pointColor: "rgba(" + _color + ",1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(" + _color + ",1)",
+                    data: $.map(_keys, function(key) {
+                        return data[key].locs[city.id].years[self.year];
+                    })
+                };
+            })
+        };
+        var ctx = $("div.widget[data-indicator='" + self.indicator + "'] canvas").get(0);
+        //
+        // $(".heading-holder[data-indicator='" + self.indicator + "']").find('h3').each(function(_, node) {
+        //     node.innerHTML = data.indicator_friendly;
+        // });
+
+        self.chart_obj = new Chart(ctx.getContext("2d"));
+        self.chart = self.init_chart(chart_data);
+        $("#legend").append(self.chart.generateLegend());
+    }
+
+    this.init_chart = function(chart_data) {
+        return this.chart_obj.Radar(chart_data, {
+            tooltipTemplate: "<%if (datasetLabel){%><%=datasetLabel%>: <%}%><%= value %>",
+            multiTooltipTemplate: "<%if (datasetLabel){%><%=datasetLabel%>: <%}%><%= value %>"
+        });
+    }
+
+    
+    return self;
+}
+OipaWheelChart.prototype = Object.create(OipaRadarChart.prototype);
+
+
+function OipaCitiesWheelChart(id, options) {
+    OipaWheelChart.call(this, id, options);
+}
+OipaCitiesWheelChart.prototype = Object.create(OipaWheelChart.prototype);
+
+
+function patch_oipa(oipa, chart_class) {
+    chart_class = chart_class == undefined ? OipaCitiesWheelChart : chart_class;
+    var _old_create_visualisations = oipa.create_visualisations;
+
+    oipa.create_visualisations = function(forced_chart_class) {
+        var thisoipa = this;
+        data = this.mainSelection.indicators;
+        //data = this.clean_blank_visualisations(data);
+
+        var chart_id = 'wheel';
+
+        if (thisoipa.visualisations[chart_id] == undefined) {
+            thisoipa.visualisations[chart_id] = new chart_class(chart_id);
+            thisoipa.visualisations[chart_id]._initial_selection = thisoipa.mainSelection;
+            thisoipa.visualisations[chart_id].selection = new OipaIndicatorSelection();
+            thisoipa.visualisations[chart_id].selection.cities = thisoipa.mainSelection.cities;
+            thisoipa.visualisations[chart_id].selection.countries = thisoipa.mainSelection.countries;
+            thisoipa.visualisations[chart_id].selection.regions = thisoipa.mainSelection.regions;
+            thisoipa.visualisations[chart_id].selection.indicators.push({"id": chart_id, "name": chart_id, "type": chart_id});
+            thisoipa.visualisations[chart_id].indicator = chart_id;
+            thisoipa.visualisations[chart_id].name = chart_id;
+            thisoipa.visualisations[chart_id].y_name = chart_id;
+            thisoipa.visualisations[chart_id].y_format = d3.format(',r');
+            thisoipa.visualisations[chart_id].x_name = 'Time (Years)';
+            thisoipa.visualisations[chart_id].x_format = d3.format('r');
+            thisoipa.visualisations[chart_id].init();
+        } else {
+            thisoipa.visualisations[chart_id].selection.cities = thisoipa.mainSelection.cities;
+            thisoipa.visualisations[chart_id].selection.countries = thisoipa.mainSelection.countries;
+            thisoipa.visualisations[chart_id].selection.regions = thisoipa.mainSelection.regions;
+            thisoipa.visualisations[chart_id].refresh();
+        }
+        return;
+    }
+}
---
+++ pages/template-compare_cpi.php
@@ -0,0 +1,138 @@
+<?php
+/*
+Template Name: CPI compare
+*/
+get_header(); the_post(); ?>
+<style>
+#visualization_wheel {
+    width: 1000px;
+}
+#visualization_wheel canvas {
+    margin-top: -50px;
+}
+#legend {
+    position: absolute; 
+    z-index: 999;
+    top: 0px;   
+    right: 10px;
+    background: #FFF;
+    padding: 10px;
+    display: inline-block;
+}
+#legend ul li {
+    list-style: none;
+    display: block;
+    width: auto;
+}
+#legend ul li span {
+    width: 20px;
+    font-size: 2px;
+    padding: 0px 10px;
+    vertical-align: middle;
+    margin-right: 10px;
+}
+</style>
+    <div id="main" class="city-compare-wrapper">
+        <!-- container-map -->
+        <div class="container-map small">
+            <?php get_template_part("compare", "filters"); ?>
+
+            <div class="container-heading">
+                <!-- container-heading -->
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 col-xs-6"><span id="compare-left-title" class="heading"></span></div>
+                    <div class="col-md-6 col-sm-6 col-xs-6"><span id="compare-right-title" class="heading"></span></div>
+                </div>
+                <!-- action-list -->
+                <ul class="action-list">
+                    <li><a id="compare-cities-randomize" href="#"><i class="icon-reset"></i>RANDOMIZE</a></li>
+                </ul>
+            </div>
+            <div class="columns-holder">
+                <div class="holder">
+                    <div id="compare-left-map-border" class="column">
+                        <?php 
+                        $curmapname = "left";
+                        include( TEMPLATEPATH .'/map.php' ); 
+                        ?>
+                    </div>
+                    <div id="compare-right-map-border" class="column">
+                        <?php 
+                        $curmapname = "right";
+                        include( TEMPLATEPATH .'/map.php' ); 
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <div class="container-text hidden-xs">
+                <div class="holder">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-6">
+                            <div class="text-box">
+                                <div class="text-frame left-city-wikipedia">
+                                    
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <div class="text-box">
+                                <div class="text-frame right-city-wikipedia">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php get_template_part("compare", "visualisations"); ?>
+
+    </div>
+
+
+    <?php get_template_part("footer", "scripts"); ?>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/oipa/wheel-chart.js"></script>
+
+    <script>
+
+        Oipa.pageType = "compare";
+        Oipa.mainSelection = new OipaCompareSelection(1);
+        patch_oipa(Oipa);
+
+        // Force refresh
+        OipaWidgetsBus.use_force_refresh = true;
+        var filter_div = "";
+
+        var leftmap = new OipaMap();
+        leftmap.set_map("left-map");
+        leftmap.compare_left_right = "left";
+        Oipa.maps.push(leftmap);
+
+        var rightmap = new OipaMap();
+        rightmap.set_map("right-map");
+        rightmap.compare_left_right = "right";
+        Oipa.maps.push(rightmap);
+
+
+        
+        var filter = new UnhabitatOipaCompareFilters();
+        filter.selection = Oipa.mainSelection;
+
+        //OipaCompare.randomize(1);
+        filter.init();
+
+
+        /*$("#compare-cities-randomize").click(function(e){
+            e.preventDefault();
+            OipaCompare.randomize(undefined, true);
+        });*/
+
+    </script>
+
+    <?php get_template_part("footer", "bus_scripts"); ?>
+
+    <script type="text/javascript">
+    //filter.save();
+    </script>
+<?php get_footer(); ?>
