f7bb0f76d58a5b90d992c4ee429ce29af823cecd
Siem <siemvaessen@gmail.com>
2014-10-03 11:46:19 +0200

    text fixes
    text fixes

--- pages/template-userdashboard.php
+++ pages/template-userdashboard.php
@@ -91,7 +91,7 @@ get_header(); the_post();
             <div class="container-custom">
                 <div class="row">
                     <div class="col-md-8 col-sm-8">
-                        <h3>My city infographics</h3>
+                        <h3>My infographics</h3>
                     </div>
                 </div>
                 <form action="" id="dash-mi-form" method="POST">
